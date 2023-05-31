9fffad66729a38ff50139eec9f48de19f8dd14a9
Érik Eiji <eriknibe1@gmail.com>
2023-05-31 13:27:07 -0300

    feat: add dashboard layout

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,66 @@
 import styled from "styled-components";
 
-export const Navbar = styled.nav`
-  height: 70px;
+export const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-  border-bottom: 1.5px solid var(--);
+  width: 90%;
+  max-width: 1000px;
+  min-height: 100px;
+
+  overflow-x: auto;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+`;
+
+export const Navbar = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom: 1.5px solid var(--color-gray-900);
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom: 1.5px solid var(--color-gray-900);
+`;
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+
+  border-collapse: collapse;
+
+  background-color: var(--color-white);
+  color: black;
+
+  th,
+  td {
+    min-width: max-content;
+    border: 1px solid black;
+    padding: 10px;
+  }
+
+  td > div {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    width: 100%;
+  }
 `;
--- src/styles/Logo.ts
+++ src/styles/Logo.ts
@@ -5,4 +5,8 @@ export const Logo = styled.h1`
   font-size: 36px;
   text-decoration-line: underline;
   text-decoration-thickness: 7px;
+
+  @media (max-width: 500px) {
+    font-size: 30px;
+  }
 `;
